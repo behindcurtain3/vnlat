02aca8d7f719d0c7c9d49a1998859536490e16d4
Justin Brown <behindcurtain3@gmail.com>
2020-11-12 14:40:06 -0800

    Update job to make PG happy

--- app/jobs/update_person_boxoffice_job.rb
+++ app/jobs/update_person_boxoffice_job.rb
@@ -6,19 +6,21 @@ class UpdatePersonBoxofficeJob < ApplicationJob
     people.each do |person|
       # sum up the box office totals of all their credited work and cache it
       result_us = Person.joins(:movies)
-                  .select('people.id, sum(boxoffice_us) as "sum_us"')
+                  .select('sum(boxoffice_us) as "sum_us"')
                   .where.not({ movies: { boxoffice_us: nil } })
                   .where.not({ appearances: { uncredited: true } })
                   .where(id: person.id)
+                  .group('people.id')
                   .first
       
       us_cache = result_us.nil? || result_us.sum_us.blank? ? 0 : result_us.sum_us
 
       result_fo = Person.joins(:movies)
-                  .select('people.id, sum(boxoffice_foreign) as "sum_foreign"')
+                  .select('sum(boxoffice_foreign) as "sum_foreign"')
                   .where.not({ movies: { boxoffice_foreign: nil } })
                   .where.not({ appearances: { uncredited: true } })
                   .where(id: person.id)
+                  .group('people.id')
                   .first
 
       foreign_cache = result_fo.nil? || result_fo.sum_foreign.blank? ? 0 : result_fo.sum_foreign
